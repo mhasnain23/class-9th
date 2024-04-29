bc56abc00ddc4d16322713707321aac4c2366d8b
mhasnain23 <tryfrelancer23@gmail.com>
2024-04-29 02:53:39 -0700

    nit

---
+++ main.ts
@@ -0,0 +1,7 @@
+// const arr: Array<string | number> = ["mango", "apple", 23, 45];
+
+// const car1: string = 'Dodge';
+// const car2: string = 'La farrari';
+// const car3: string = 'Lambo';
+
+let carsName = []
